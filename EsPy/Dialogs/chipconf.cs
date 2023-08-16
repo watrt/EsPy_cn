b85d9bc16bbc9a88e126c54d84782c05d3033eac
watrt <xb100@qq.com>
2023-08-16 11:53:36 +0800

    Squashed commit of the following:
    commit 101080efaaabc2347b80d86838add81c2ebdb381
    Author: watrt <xb100@qq.com>
    Date:   Wed Aug 16 11:25:09 2023 +0800
    
        更新模拟器内容
    
        更新模拟器内容
    
    commit 35c80f036f293ee9e49a1b39db9889cc7abf9ab2
    Author: watrt <xb100@qq.com>
    Date:   Fri Jul 28 14:53:34 2023 +0800
    
        添加LVGL防真器
    
    commit 1168c02e27c329a05a60853d7ff08a44abca6990
    Author: watrt <xb100@qq.com>
    Date:   Fri Jul 28 12:32:15 2023 +0800
    
        添加虚拟机
    
        添加虚拟机
    
    commit b2a936ab4249423ce8d84f6e2f9843590aad7459
    Author: watrt <xb100@qq.com>
    Date:   Fri Jun 2 12:14:31 2023 +0800
    
        添加面板分支

---
+++ EsPy/Dialogs/chipconf.cs
@@ -0,0 +1,71 @@
+﻿using EsPy.Units;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsPy.Dialogs
+{
+    public partial class chipconf : Form
+    {
+        public chipconf()
+        {
+            InitializeComponent();
+        }
+        public PySerial Port
+        { get; set; }
+        private void chipconf_Load(object sender, EventArgs e)
+        {
+            if (this.Port != null && this.Port.IsOpen)
+            {
+                ResultStatus res = this.Port.Getfreq();
+                if (res.Result == ResultStatus.Statuses.Success)
+                {
+                    try
+                    {
+                        string freq=(int.Parse(res.ToString())/ 1000000).ToString();
+                        Console.WriteLine(res.ToString());
+                        comboBox1.Text = freq + "MHZ";
+                    }
+                    catch
+                    {
+
+                    }
+                    
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.Port != null && this.Port.IsOpen)
+            {
+                ResultStatus res = this.Port.Getwifi();
+                if (res.Result == ResultStatus.Statuses.Success)
+                {
+                    wifiname.Items.Clear();
+                    foreach(wificlient i in res.Data as List<wificlient>)
+                    {
+                        wifiname.Items.Add(i.getname);
+                    }
+                    Console.WriteLine(res.ToString());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
